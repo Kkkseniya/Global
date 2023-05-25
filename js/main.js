6a7b0806acd1922f4207f406c57d5170a00cd7c4
Kkkseniya <kenya020@mail.ru>
2023-05-26 00:25:36 +0300

    added mobile version

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
 const formInput = document.querySelector(".form-input");
 const description = document.querySelector(".info__description");
+const burgerMenu = document.querySelector(".burger-menu");
+const headerMobile = document.querySelector(".header-mobile");
 
 //regular expression fot input
 formInput.addEventListener("input", (e) => {
   e.target.value = e.target.value.replace(/[\!\@\#\$\%\^\&\*\(\)]/g, "");
+});
+
+burgerMenu.addEventListener("click", () => {
+  headerMobile.classList.toggle("header-mobile--visible");
 });
 
 //getting data for description
